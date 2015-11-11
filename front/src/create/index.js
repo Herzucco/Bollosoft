3c1f50e1b5639aeec7c4a88a023f227f26296cc5
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-11-11 16:15:13 +0100

    Fit canvas to screen

--- front/src/create/index.js
+++ front/src/create/index.js
@@ -6,6 +6,13 @@ function create(){
 	testAnim = game.add.sprite(10, 10, 'testFace', 'face/anim/1.png');
 	testAnim.animations.add('change', Phaser.Animation.generateFrameNames('face/anim/', 1, 3, '.png', 1), 10, true, false);
 	testAnim.animations.play('change');
+
+	game.scale.maxWidth = 1920;
+	game.scale.maxHeight = 1080;
+	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	game.scale.offset = 0;
+	game.debug.inputInfo(16,16);
+
 	loadLayouts();
 }
 
--- front/src/game.js
+++ front/src/game.js
@@ -1,6 +1,6 @@
 function game(preload, create, loop){
   console.log(loop);
-  return new window.Phaser.Game(800, 600, Phaser.AUTO, 'Bolloqués', { preload: preload, create: create, render: loop });
+  return new window.Phaser.Game(1920, 1080, Phaser.AUTO, 'Bolloqués', { preload: preload, create: create, render: loop });
 }
 
 module.exports = game;
