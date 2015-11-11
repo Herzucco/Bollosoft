9670b22e34187231c87a5aade671c22af9cc3f9c
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-11-11 16:25:54 +0100

    Made my own layout

--- front/src/create/index.js
+++ front/src/create/index.js
@@ -1,12 +1,6 @@
 var loadLayouts = require('./layouts-handler');
-var testAnim;
 
 function create(){
-	game.add.sprite(0, 0, 'testFace', 'waifu.png');
-	testAnim = game.add.sprite(10, 10, 'testFace', 'face/anim/1.png');
-	testAnim.animations.add('change', Phaser.Animation.generateFrameNames('face/anim/', 1, 3, '.png', 1), 10, true, false);
-	testAnim.animations.play('change');
-
 	game.scale.maxWidth = 1920;
 	game.scale.maxHeight = 1080;
 	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
---
+++ front/src/layouts/greugProtoPannel.js
@@ -0,0 +1,20 @@
+var Layout = require('./layout');
+var testAnim;
+
+function GreugProtoPannel(){
+	Layout.call(this);
+	game.add.sprite(0, 0, 'testFace', 'waifu.png');
+	testAnim = game.add.sprite(10, 10, 'testFace', 'face/anim/1.png');
+	testAnim.animations.add('change', Phaser.Animation.generateFrameNames('face/anim/', 1, 3, '.png', 1), 10, true, false);
+	testAnim.animations.play('change');
+}
+
+GreugProtoPannel.prototype = Object.create(Layout.prototype);
+GreugProtoPannel.prototype.constructor = GreugProtoPannel;
+
+GreugProtoPannel.prototype.update = function GreugProtoPannelUpdate(game){
+	Layout.prototype.update.call(this, game);
+	console.log("greug pannel");
+}
+
+module.exports = GreugProtoPannel;
