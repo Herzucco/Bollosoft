c21545c6fdfa0ba92b49315bc1485d42df7c7ef1
Adrien Carta <herzucco@gmail.com>
2015-12-03 11:55:11 +0100

    add slides and presentator events

--- front/src/dialogs/Dialog.js
+++ front/src/dialogs/Dialog.js
@@ -12,6 +12,7 @@ function Dialog(group, language, position, maxSentences, spacing){
   this.currentSentence = -1;
 
   this.characters = {};
+  this.settings = {};
 
   this.bubbles = [];
 
@@ -23,15 +24,17 @@ function Dialog(group, language, position, maxSentences, spacing){
 
 Dialog.prototype.load = function LoadDialog(text){
   this.computeDialog(text);
-  this.characters = text.characters;
 
-  this.next(1);
+  this.next(1, true);
 }
 
 Dialog.prototype.computeDialog = function ComputeDialog(text){
   for(var i = 0; i < text.text.length; i++){
     this.sentences.push(text.text[i]);
   }
+
+  this.characters = text.characters;
+  this.settings = text.settings;
 }
 
 Dialog.prototype.createBubble = function CreateBubbleDialog(){
@@ -44,14 +47,33 @@ Dialog.prototype.createBubble = function CreateBubbleDialog(){
   bubble.start();
 }
 
-Dialog.prototype.next = function ForwardDialog(delta){
+Dialog.prototype.next = function ForwardDialog(delta, sentenceComputing){
   if(this.currentSentence + delta < this.sentences.length){
     this.currentSentence += delta;
 
-    this.createBubble();
+    if(sentenceComputing){
+      this.createBubble();
+    }
+
   }else{
     this.canInput = false;
     window.game.events.emit('choiceStart');
+  }
+}
+
+Dialog.prototype.computeSentence = function ComputeSentenceDialog(sentence){
+  switch(sentence[this.language]){
+    case '$$$' :
+      window.game.events.emit('slide', this.settings.picture);
+      return false;
+    break;
+    case '@@@' :
+      window.game.events.emit('presentator', this.settings.presentator);
+      return false;
+    break;
+    default :
+      return true;
+    break;
   }
 }
 
@@ -72,13 +94,18 @@ Dialog.prototype.waitInput = function WaitInputDialog(){
     this.skippable = false;
 
     if(bubble.isOver){
+      var sentenceComputing;
+
       if(this.currentSentence + 1 < this.sentences.length){
-        for(var i = 0; i < this.bubbles.length; i++){
-          this.bubbles[i].up();
+        sentenceComputing = this.computeSentence(this.sentences[this.currentSentence + 1]);
+        if(sentenceComputing){
+          for(var i = 0; i < this.bubbles.length; i++){
+            this.bubbles[i].up();
+          }
         }
       }
 
-      this.next(1);
+      this.next(1, sentenceComputing);
     }else{
       bubble.skip();
     }
