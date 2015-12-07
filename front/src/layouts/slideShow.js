1bb9fb01cb1428628e61e320c9307a0f951ef24d
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-12-07 11:38:37 +0100

    Début de placement de trucs

--- front/src/layouts/slideShow.js
+++ front/src/layouts/slideShow.js
@@ -1,14 +1,30 @@
 var Layout = require('./layout');
+var devShadow;
+var devAnim;
+//var wobbling;
+var slideBack;
 
 function SlideShow(){
   Layout.call(this);
+  this.enable();
 
+  game.stage.backgroundColor = "#595858";
+  devShadow = game.add.sprite(-250, 200, 'proto', 'proto/devShadow.png');
+  devAnim = game.add.tween(devShadow);
+  devAnim.to({x:400}, 200, Phaser.Easing.Linear.None);
+  //devAnim.onComplete.add();
+  devAnim.start();
+  this.group.add(devShadow);
+
+  slideBack = game.add.sprite(0, 200, 'proto', 'proto/PurpleBubble.png');
 }
 
 SlideShow.prototype = Object.create(Layout.prototype);
 SlideShow.prototype.constructor = SlideShow;
 SlideShow.prototype.update = function SlideShowUpdate(game){
   Layout.prototype.update.call(this, game);
+  //wobbling = Math.sin(game.time.now) * 20;
+  //devShadow.y = wobbling;
 }
 
 module.exports = SlideShow;
