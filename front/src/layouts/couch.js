e82edb0aec372b5b72897ba73ab204f104443873
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2016-01-09 18:10:51 +0100

    Truc cassé

--- front/src/layouts/couch.js
+++ front/src/layouts/couch.js
@@ -2,6 +2,10 @@ var Layout = require('./layout');
 var rightBackground;
 var bollo;
 var guigui;
+var stopDuration = 0;
+var stopChrono = 0;
+var peopleStoping;
+var animRhythm = 0;
 
 function Couch(){
   	Layout.call(this);
@@ -13,13 +17,13 @@ function Couch(){
   	bollo = game.add.sprite(1200, 600, 'bolloSprite');
 	bollo.animations.add('idleAnim', Phaser.Animation.generateFrameNames('idle', 1, 4, '.png', 1), 10, true, false);
 	bollo.animations.add('talkingAnim', Phaser.Animation.generateFrameNames('talking', 1, 4, '.png', 1), 10, true, false);
-	bollo.animations.play('idleAnim', 6, true);
+	bollo.animations.play('idleAnim', 5, true);
 	this.group.add(bollo);
 
 	guigui = game.add.sprite(1500, 650, 'guiguiSprite');
 	guigui.animations.add('idleAnim', Phaser.Animation.generateFrameNames('idle', 1, 4, '.png', 1), 10, true, false);
 	guigui.animations.add('talkingAnim', Phaser.Animation.generateFrameNames('talking', 1, 4, '.png', 1), 10, true, false);
-	guigui.animations.play('idleAnim', 6, true);
+	guigui.animations.play('idleAnim', 5, true);
 	this.group.add(guigui);
 
 	var that = this;
@@ -28,6 +32,9 @@ function Couch(){
 	});
 	window.game.events.on('endTalking', function(peopleShutUp){
     	that.endTalk(peopleShutUp);
+	});
+	window.game.events.on('pauseTalking', function(peopleStop){
+    	that.pauseTalk(peopleStop);
 	});
 }
 
@@ -38,13 +45,23 @@ var mapRange = function(from, to, s) {
 Couch.prototype = Object.create(Layout.prototype);
 Couch.prototype.constructor = Couch;
 Couch.prototype.update = function CouchUpdate(game){
-  Layout.prototype.update.call(this, game);
+	Layout.prototype.update.call(this, game);
+	stopChrono += 1/60;
+	if (stopChrono >= stopDuration)
+	{
+		if (peopleStoping != null)
+		{
+			peopleStoping.animations.play('talkingAnim', mapRange([0, 1], [10, 4], 0), true);
+		}
+	}
+	//console.log(stopChrono);
 }
 
 Couch.prototype.startTalk = function StartTalkAnim(peopleTalking){
-	console.log("wesh " + peopleTalking.character.rythm);
+	//console.log("wesh " + peopleTalking.character.rythm);
 	//if (peopleTalking.noAnim == false)
 	//{
+		animRhythm = peopleTalking.rythm;
 		if (peopleTalking.name == "Bolloré")
 		{
 			bollo.animations.play('talkingAnim', mapRange([0, 1], [10, 4], peopleTalking.character.rythm), true);
@@ -60,12 +77,30 @@ Couch.prototype.startTalk = function StartTalkAnim(peopleTalking){
 Couch.prototype.endTalk = function EndTalkAnim(peopleShutUp){
 	if (peopleShutUp.name == "Bolloré")
 	{
-		bollo.animations.play('idleAnim', 6, true);
+		bollo.animations.play('idleAnim', 5, true);
 		bollo.x = 1200;
 	}
 	if (peopleShutUp.name == "Guillemot")
 	{
-		guigui.animations.play('idleAnim', 6, true);
+		guigui.animations.play('idleAnim', 5, true);
+	}
+}
+
+Couch.prototype.pauseTalk = function PauseTalkAnim(peopleStop){
+	stopChrono = 0;
+	stopDuration = peopleStop.duration;
+	peopleStoping == peopleStop.name;
+
+	if (peopleStop.name == "Bolloré")
+	{
+		peopleStoping = bollo;
+		bollo.animations.stop(null, true);
+	}
+	if (peopleStop.name == "Guillemot")
+	{
+		console.log("wesh " + stopChrono + " & " + peopleStop.duration);
+		peopleStoping = guigui;
+		guigui.animations.stop(null, true);
 	}
 }
 
