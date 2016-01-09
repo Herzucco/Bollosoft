62cea35a0b3567611c4bfb97d25719116e4e6b98
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2016-01-09 16:03:28 +0100

    Ca parle mal

--- front/src/layouts/couch.js
+++ front/src/layouts/couch.js
@@ -21,12 +21,43 @@ function Couch(){
 	guigui.animations.add('talkingAnim', Phaser.Animation.generateFrameNames('talking', 1, 4, '.png', 1), 10, true, false);
 	guigui.animations.play('idleAnim', 6, true);
 	this.group.add(guigui);
+
+	var that = this;
+	window.game.events.on('startTalking', function(peopleTalking){
+    	that.startTalk(peopleTalking);
+	});
+	window.game.events.on('endTalking', function(peopleShutUp){
+    	that.endTalk(peopleShutUp);
+	});
 }
 
 Couch.prototype = Object.create(Layout.prototype);
 Couch.prototype.constructor = Couch;
 Couch.prototype.update = function CouchUpdate(game){
   Layout.prototype.update.call(this, game);
+}
+
+Couch.prototype.startTalk = function StartTalkAnim(peopleTalking){
+	console.log("wesh " + peopleTalking.character.rythm);
+	if (peopleTalking.name == "Bolloré")
+	{
+		guigui.animations.play('talkingAnim', 6, true);
+	}
+	if (peopleTalking.name == "Guillemot")
+	{
+		guigui.animations.play('talkingAnim', 6, true);
+	}
+}
+
+Couch.prototype.endTalk = function EndTalkAnim(peopleShutUp){
+	if (peopleShutUp.name == "Bolloré")
+	{
+		guigui.animations.play('idleAnim', 6, true);
+	}
+	if (peopleShutUp.name == "Guillemot")
+	{
+		guigui.animations.play('idleAnim', 6, true);
+	}
 }
 
 module.exports = Couch;
